1b16dce627fd903d3e5f4c56f0683c528887e926
Pablo Seibelt <pabloseibelt@sicarul.com>
2015-12-19 18:47:07 -0300

    Remove obsolete dollars

--- bluelytics/dolar_blue/api.py
+++ bluelytics/dolar_blue/api.py
@@ -19,20 +19,21 @@ def all_prices():
   all_sources = Source.objects.all()
   allPrices = []
   for src in all_sources:
-    today = DolarBlue.objects.filter(source__exact=src).order_by('-date').first()
-    dateCalc = today.date.replace(hour=3, minute=0, second=0)
-    yesterday = DolarBlue.objects.filter(source__exact=src, date__lt=dateCalc).order_by('-date').first()
-    if not yesterday:
-      yesterday = today
-    allPrices.append({
-      'date': today.date.astimezone(arg).isoformat(),
-      'compra': today.value_buy,
-      'venta': today.value_sell,
-      'compra_ayer': yesterday.value_buy,
-      'venta_ayer': yesterday.value_sell,
-      'name': today.source.source,
-      'long_name': today.source.description
-      })
+    if str(src) != 'la_nacion':
+        today = DolarBlue.objects.filter(source__exact=src).order_by('-date').first()
+        dateCalc = today.date.replace(hour=3, minute=0, second=0)
+        yesterday = DolarBlue.objects.filter(source__exact=src, date__lt=dateCalc).order_by('-date').first()
+        if not yesterday:
+          yesterday = today
+        allPrices.append({
+          'date': today.date.astimezone(arg).isoformat(),
+          'compra': today.value_buy,
+          'venta': today.value_sell,
+          'compra_ayer': yesterday.value_buy,
+          'venta_ayer': yesterday.value_sell,
+          'name': today.source.source,
+          'long_name': today.source.description
+          })
 
   return allPrices
 
@@ -45,9 +46,9 @@ def avgBlue(input):
   d = 0
   blue = filter(lambda x: x['name'] != 'oficial', input)
   return {'date': datetime.datetime.now().isoformat(),
-        'compra': median(map(lambda x: x['venta'], blue)) * Decimal(buy_multiplier),
+        'compra': median(map(lambda x: x['venta'], blue)) * buy_multiplier,
         'venta': median(map(lambda x: x['venta'], blue)),
-        'compra_ayer': median(map(lambda x: x['venta_ayer'], blue))*Decimal(buy_multiplier),
+        'compra_ayer': median(map(lambda x: x['venta_ayer'], blue))* buy_multiplier,
         'venta_ayer': median(map(lambda x: x['venta_ayer'], blue)),
         'name': 'blue',
         'long_name': 'Dolar Blue'
@@ -76,10 +77,6 @@ def lastprice(request):
   output.append(avgBlue(allPrices))
 
   output+=allPrices
-
-  output.append(addOficial(allPrices, 20, 'Oficial Ahorro'))
-
-  output.append(addOficial(allPrices, 35, 'Oficial Tarjeta'))
 
   return HttpResponse(json.dumps(output, cls=DecimalEncoder), mimetype="application/json")
 
