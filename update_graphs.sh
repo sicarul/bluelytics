ed28a08cc0728277f3a53960e33da5a602157245
Pablo Seibelt <pabloseibelt@sicarul.com>
2020-04-24 00:49:27 -0300

    Add new method for currencies

--- update_graphs.sh
+++ update_graphs.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/bin/activate
 python $DIR/bluelytics/manage.py export_graph bluelytics/data/graphs/evolution.json
+python $DIR/bluelytics/manage.py export_currencies bluelytics/data/json/currency.json
