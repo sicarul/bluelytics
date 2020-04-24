c02420c211547ec2fb95e3cef8b77db3fc81de4e
Pablo Seibelt <pabloseibelt@sicarul.com>
2020-04-24 01:57:40 -0300

    Fix

--- update_graphs.sh
+++ update_graphs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+set -e
 . $DIR/bin/activate
 python $DIR/bluelytics/manage.py export_graph bluelytics/data/graphs/evolution.json
 python $DIR/bluelytics/manage.py export_currencies bluelytics/data/json/currency.json
